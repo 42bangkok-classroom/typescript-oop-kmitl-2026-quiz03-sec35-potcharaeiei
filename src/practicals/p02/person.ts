9f93b723dd4ca506bea8ed07d270ba71627e194b
potcharaeiei <68010736@kmitl.ac.th>
2026-02-16 09:59:59 +0700

    02

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,18 +1,19 @@
 export class Person {
     firstname?: string = "";
     lastname?: string = "";
-    private age?: number = 0;
+    age?: number = 0;
+
     static COUNTRY: string = "Thailand";
-
-    printName(): void {
-        console.log(`${this.firstname} ${this.lastname}`);
-    }
-
-    printAge(): void {
-        console.log(`${this.age}`);
-    }
 
     setAge(age: number): void {
         this.age = age;
+    }
+
+    getAge(): number {
+        return this.age as number;
+    }
+
+    getFullName(): string {
+        return `${this.firstname} ${this.lastname}`;
     }
 }
